ed920fec8d67d450dd2becdff75c3b2341dc8686
chimela-caesar <chimela.caesar@gmail.com>
2017-05-18 18:12:39 +0100

    Add files to repository

--- app/findMissing.js
+++ app/findMissing.js
@@ -5,7 +5,12 @@ var exports = module.exports = {};
 exports.findMissing = function(arr1, arr2)
 {
   if (arr1 && arr1.length === 0 || arr2 && arr2.length === 0) return 0;
-  if (arr1 === arr2) return 0;
+  
+  let equal = arr1.every(function(item, index, array){
+    return (array[index] === arr2[index]);
+  });
+  
+  if (equal) return 0;
    
   let largeArr, smallArr;
   
